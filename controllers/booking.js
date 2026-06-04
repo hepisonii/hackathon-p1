1788788b69e01c1b361f7f65e669c7d0520ca846
Hepi Soni <hepisonii@gmail.com>
2026-06-04 16:45:41 +0530

    env file hided

--- controllers/booking.js
+++ controllers/booking.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const path = require("path");
-
+const User = require("../models/user");
 async function handleGetBooking(req,res){
     return res.sendFile(path.resolve(__dirname, "../views/book_venue.html"));
 }
 
 async function handlePostBooking(req,res){
-    return res.sendFile(path.resolve(__dirname, "../views/book_venue.html"));
+    const sport = req.body;
+    const city = req.user.city;
+    const venues = await User.find(
+  {
+    role: "venue-owner",
+    sports: { $in: sport },
+    city
+  },
+  {
+    venue_name: 1,
+    venue_address: 1,
+    fullname: 1,
+    _id: 1
+  }
+);
+    console.log("Venues", venues);
+    return res.json({
+        venues
+    });
 }
 
 module.exports = {
