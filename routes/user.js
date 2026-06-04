a9093809ebf396450252b6aef53353336942c140
Hepi Soni <hepisonii@gmail.com>
2026-06-04 10:35:17 +0530

    Initial commit

---
+++ routes/user.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const userRouter = express.Router();
+const {upload} = require("../cloudConfig")
+const {
+    handleGetUserSignUp,
+    handlePostUserSignUp,
+    handleGetUserLogin,
+    handlePostUserLogin,
+    handleGetUserLogout,
+} = require("../controllers/user");
+const { limiter } = require("../middlewares/auth");
+
+
+userRouter.get("/signup",handleGetUserSignUp);
+userRouter.post("/signup",upload.single("photo"), handlePostUserSignUp);
+userRouter.get("/login", handleGetUserLogin);
+userRouter.post("/login",limiter, handlePostUserLogin);
+userRouter.get("/logout", handleGetUserLogout);
+
+module.exports = userRouter;
